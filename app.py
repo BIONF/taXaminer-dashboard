8110440d0f83f40218a77b1952449591666cb711
Lucas Koch <lkcontact01@gmail.com>
2021-12-16 17:17:49 +0100

    Layout Prototype

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import dash_bootstrap_components as dbc
 import math
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output, State
+from dash import dash_table
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 import data_io as milts_files
+
 import numpy as np
 import sys
 import yaml
@@ -18,7 +20,15 @@ import yaml
 # config_obj = yaml.safe_load(open(config_path, 'r'))
 # output_path=config_obj.get('output_path')# complete path to output directory
 
-""" First compile dataset overview"""
+"""
+DIRECTORY FORMAT:
+./data/<name of MILTS run>/gene_info
+./data/<name of MILTS run>/PCA_and_clustering
+./data/<name of MILTS run>/taxonomic_assignment
+./data/<name of MILTS run>/proteins.faa
+./data/<name of MILTS run>/taxonomic_hits.txt
+"""
+
 output_path = "./data/"
 base_path = "./data/"
 datasets = []
@@ -33,23 +43,15 @@ print("=== Finished File Discovery ===")
 
 pca_data = pd.read_csv(datasets[0] + "PCA_and_clustering/PCA_results/pca_summary.csv")
 
-# prep data
-pca_resolution = 5
-proportion_of_variance = []
-pca_ids = []
-for i in range(1, pca_resolution + 1):
-    curr_value = pca_data.get("PC" + str(i))
-    pca_ids.append("PC" + str(i))
-    proportion_of_variance.append(curr_value[1])
-
-# Compile dataframe and graph
-pca_data = pd.DataFrame(proportion_of_variance, pca_ids)
-pca_fig = px.bar(pca_data, width=300)
-pca_fig.update_layout(showlegend=False)
-
 # global dataframe
 path = datasets[0]
 data = pd.read_csv(datasets[0] + "taxonomic_assignment/gene_table_taxon_assignment.csv")
+
+scatter_test = px.scatter_matrix(data, dimensions=['Dim.1', 'Dim.2', 'Dim.3'])
+
+# TODO change architecture to use dict for different dataframes
+data_frames = {'base': data, 'selection': data}
+selected_genes = []
 
 # Init app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -65,140 +67,192 @@ app.layout = dbc.Container(fluid=True, children=[
     ),
     dbc.Row(
         [
-            dbc.Col(width=9, children=[
+            # wide scatterplot
+            dbc.Col(width=8, children=[
                 dcc.Graph(
                     id="scatter3d",
                     config={"displayModeBar": True},
                     animate=True,
                     className="plot",
                 )]),
-            dbc.Col(children=[
+
+            # tabbed side menu
+            dbc.Col(width=4, children=[
                 dbc.Tabs(children=[
                     dbc.Tab(label='Overview',
                             children=[
                                 # Dataset Selection
-                                dbc.Row([
-                                    html.H5("Select Dataset"),
-                                    dcc.Dropdown(
-                                        id='dataset_select',
-                                        options=dropdowns,
-                                        value=dropdowns[0].get('value')
-                                    )
-                                ]),
-                                # Taxon Information
-                                dbc.Row([
-                                    html.H5(children="General Information"),
-                                    dcc.Textarea(
-                                        id='textarea-taxon',
-                                        value='Textarea content initialized\nwith multiple lines of text',
-                                        disabled=True,
-                                        style={'height': 200, 'width': 'fill', "verticalAlign": "top",
-                                               'horizontalAlign': 'left',
-                                               'horizontalAlign': 'left',
-                                               'marginLeft': '11px'},
-                                    ),
-                                ]),
+                                html.Hr(),
                                 dbc.Row([
                                     dbc.Col([
-                                        # Sequence data
-                                        html.H5("Sequence Data"),
-                                        html.Div(
-                                            [
-                                                dbc.Button(
-                                                    "View Raw Sequence",
-                                                    id="collapse-button",
-                                                    color="primary",
-                                                    n_clicks=0,
-                                                    className="d-grid col-12 mb-3",
-                                                ),
-                                                dbc.Collapse(
-                                                    dbc.Card(
-                                                        dbc.CardBody([
-                                                            dcc.Textarea(
-                                                                id='textarea-sequence',
-                                                                value='Textarea',
-                                                                disabled=True,
-                                                                style={'width': '100%', 'height': 300,
-                                                                       "verticalAlign": "top",
-                                                                       'horizontalAlign': 'center'},
-                                                            ),
-                                                        ])),
-                                                    id="collapse",
-                                                    is_open=False,
-                                                ),
-                                            ]
-                                        ),
-                                    ]),
-                                ], align='center'),
-                            ]),
-                    dbc.Tab(label='Filter',
-                            children=[
+                                        dbc.Card([
+                                            dbc.CardHeader("Select Dataset"),
+                                            dcc.Dropdown(
+                                                id='dataset_select',
+                                                options=dropdowns,
+                                                value=dropdowns[0].get('value')
+                                            )
+                                        ])
+                                    ], align='center')
+                                ], align='end'),
+                                html.Hr(),
+                                # display gene information
                                 dbc.Row([
-                                    # Gene search
-                                    dbc.Col(align='end', children=[
-                                        html.H4("Search gene identifier"),
-
-                                        dbc.Col(
-                                            dbc.Input(id='searchbar',
-                                                      placeholder="Enter Gene Name",
-                                                      invalid=True),
-                                            className="mb-3",
-                                        ),
-                                        dbc.Col(dbc.Button("Submit", color="primary", id='clear-search'),
-                                                width="auto", className='mb-3'),
+                                    dbc.Col([
+                                        dbc.Card([
+                                            dbc.CardHeader("Selected Gene"),
+                                            dcc.Textarea(
+                                                id='textarea-taxon',
+                                                value='Textarea content initialized\nwith multiple lines of text',
+                                                disabled=True,
+                                                style={'height': 200, 'width': 'fill', "verticalAlign": "top",
+                                                       'horizontalAlign': 'left'},
+                                            ),
+                                        ]),
                                     ]),
                                 ]),
-                                # e-value Filter
-                                html.H5(children="Filter by e-value",
-                                        style={"text-align": "center"}),
-                                dbc.Row([
-                                    dcc.Slider(
-                                        id='evalue-slider',
-                                        min=0,
-                                        max=300,
-                                        value=0,
-                                        step=10,
-                                        marks={0: {'label': 'e^0', 'style': {'color': '#77b0b1'}},
-                                               100: {'label': 'e^-100', 'style': {'color': '#77b0b1'}},
-                                               200: {'label': 'e^-200', 'style': {'color': '#77b0b1'}},
-                                               300: {'label': 'e^-300', 'style': {'color': '#77b0b1'}}},
-                                    ),
-                                ]),
-                            ]),
-                    dbc.Tab(label='Metrics', children=[
-                        html.Div([
-                            dcc.Graph(figure=pca_fig)
-                        ], style={'display': 'inline-block', 'width': '70%', 'horizontalAlign': 'center'})
+                            ], className="m-2"),
+                    # scatter matrix
+                    dbc.Tab(label='Scatter Matrix', children=[
+                        dbc.Row([
+                            dbc.Col([
+                                dcc.Graph(id='scatter_matrix',
+                                          figure=scatter_test,
+                                          responsive=True),
+                            ])
+                        ]),
                     ]),
                 ]),
             ]),
         ]
     ),
+    html.Hr(),
+    dbc.NavbarSimple(
+        brand="Data Selection",
+        brand_href="#",
+        color="primary",
+        dark=True,
+        fluid=True,
+    ),
+    dbc.Row([
+        dbc.Col(width=8, children=[
+            dbc.Tabs([
+                dbc.Tab([
+                    dbc.Card([
+                        dash_table.DataTable(
+                            id='table',
+                            columns=[{"name": "Gene Name", "id": "g_name"},
+                                     {"name": "Best Hit", "id": "best_hit"},
+                                     {"name": "e-value", "id": "bh_evalue"}],
+                            data=data.to_dict('records'),
+                        ),
+                    ], className="m-2"),
+                ], label="Selected Data"),
+
+                dbc.Tab([
+                    dbc.Card([
+                        dash_table.DataTable(
+                            id='table_all',
+                            columns=[{"name": "Gene Name", "id": "g_name"},
+                                     {"name": "Best Hit", "id": "best_hit"},
+                                     {"name": "e-value", "id": "bh_evalue"}],
+                            data=data.to_dict('records'),
+                        ),
+                    ], className="m-2"),
+                ], label="Full Dataset")
+            ]),
+        ]),
+        dbc.Col([
+            dbc.Tabs([
+                dbc.Tab([
+                    dbc.Card([
+                        dbc.CardHeader("Selection Options"),
+                        html.Div([
+                            dbc.ButtonGroup(
+                                [dbc.Button("Add", color="success", size="md"),
+                                 dbc.Button("Remove", color="danger", size="md")],
+                                className="d-flex m-1 radio-group btn-block"
+                            )
+                        ]),
+                        html.Div([
+                            dbc.Button("Reset entire Selection", color="danger"),
+                        ], className="d-grid gap-2 m-1"),
+                    ], className="m-2"),
+                    dbc.Card([
+                        dbc.CardHeader("Search by name"),
+                        dbc.Input(
+                            id='searchbar',
+                            placeholder="Enter Gene Name",
+                            invalid=True
+                        ),
+                    ], className="m-2"),
+                ], label="Options", className="m-2"),
+                dbc.Tab([
+                    dbc.Card([
+                        dbc.CardHeader("Enable/Disable Filters"),
+                        dbc.Checkbox(label="Scatterplot legend", className="m-1 form-switch"),
+                        dbc.Checkbox(label="e-value", className="m-1 form-switch"),
+                        dbc.Checkbox(label="Ignore unassigned", className="m-1 form-switch"),
+                    ], className="m-2"),
+                    dbc.Card([
+                        dbc.CardHeader("e-value Filter"),
+                        dcc.Slider(
+                            id='evalue-slider',
+                            min=0,
+                            max=300,
+                            value=0,
+                            step=10,
+                            marks={0: {'label': 'e^0', 'style': {'color': '#77b0b1'}},
+                                   100: {'label': 'e^-100', 'style': {'color': '#77b0b1'}},
+                                   200: {'label': 'e^-200', 'style': {'color': '#77b0b1'}},
+                                   300: {'label': 'e^-300', 'style': {'color': '#77b0b1'}}},
+                            className="m-2",
+                        ),
+                    ], className="m-2"),
+                ], label="Filter", className="m-2"),
+                # Download Tab
+                dbc.Tab([
+                    dbc.Card([
+                        dbc.CardHeader("Amino Acid sequence"),
+                        dcc.Textarea(
+                            id='textarea-sequence',
+                            value='Textarea',
+                            disabled=True,
+                            style={'height': 300},
+                        ),
+                    ], className="m-2"),
+                ], label="Download")
+            ]),
+        ], width=4)
+    ]),
 ])
 
 
 # Hover event
 @app.callback(
     Output('textarea-taxon', 'value'),
-    Input('scatter3d', 'hoverData'),
+    Input('scatter3d', 'clickData'),
     Input('searchbar', 'value'))
-def print_hover_data(hover_data, search_data):
+def print_hover_data(click_data, search_data):
     """
     Update Basic Information
-    :param hover_data:
+    :param click_data:
     :param search_data:
     :return:
     """
-    if not hover_data:
-        return "Hover of a data point to select it"
+    if not click_data:
+        return "Click a data point to select it"
 
     # Allow user search
     if search_data:
         my_point = search_data
     else:
-        my_point = hover_data['points'][0]['customdata'][0]
+        my_point = click_data['points'][0]['customdata'][0]
 
     # filter data frame
+    global selected_genes
+    selected_genes.append(my_point)
+
     gene_data = data.loc[data['g_name'] == my_point]
 
     output_text = ""
@@ -214,7 +268,7 @@ def print_hover_data(hover_data, search_data):
 
 @app.callback(
     Output('textarea-sequence', 'value'),
-    Input('scatter3d', 'hoverData'),
+    Input('scatter3d', 'clickData'),
     Input('searchbar', 'value'))
 def print_seq_data(hover_data, search_data):
     """
@@ -240,8 +294,9 @@ def print_seq_data(hover_data, search_data):
 
 @app.callback(
     Output('scatter3d', 'figure'),
+    Output('scatter_matrix', 'figure'),
     Input('evalue-slider', 'value'),
-    Input('dataset_select', 'value')
+    Input('dataset_select', 'value'),
 )
 def update_dataframe(value, new_path):
     """
@@ -260,32 +315,14 @@ def update_dataframe(value, new_path):
     my_fig.update_traces(marker=dict(size=3), hovertemplate="<br>".join([
         "%{customdata[0]}"
     ]))
-    my_fig.update_layout(legend={'itemsizing': 'constant'}, legend_title_text='Taxa')
-    return my_fig
 
+    my_fig.update_layout(legend={'itemsizing': 'constant'},
+                         legend_title_text='Taxa')
 
-@app.callback(
-    Output('searchbar', 'value'),
-    Input('clear-search', 'n_clicks'))
-def reset_searchbar(n_clicks):
-    return None
-
-
-@app.callback(
-    Output("collapse", "is_open"),
-    [Input("collapse-button", "n_clicks")],
-    [State("collapse", "is_open")],
-)
-def toggle_collapse(n, is_open):
-    """
-    Toggle Sequence information collapsible Card
-    :param n:
-    :param is_open:
-    :return:
-    """
-    if n:
-        return not is_open
-    return is_open
+    scatter_side = px.scatter_matrix(my_data,
+                                     dimensions=['Dim.1', 'Dim.2', 'Dim.3'],
+                                     custom_data=['g_name'])
+    return my_fig, scatter_side
 
 
 if __name__ == "__main__":
